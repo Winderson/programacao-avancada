c452df78512d01578b5b2963d3fa4b0872ca41ef
Winderson <winderson.wjbs@gmail.com>
2018-04-11 21:37:45 -0300

    Criação trabalho1

---
+++ trabalho1/cmake-build-debug/CMakeFiles/trabalho1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho1.dir/main.cpp.o"
+  "trabalho1.pdb"
+  "trabalho1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabalho1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
